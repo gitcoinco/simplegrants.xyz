dc9a3098ae1210773662348bc3c1feabe2409c6a
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-02-27 12:25:54 +0100

    Update round schema and update funded amount

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -16,56 +16,22 @@ async function handler(req: NextRequest) {
       env.STRIPE_WEBHOOK_SECRET,
     );
 
-    Object.assign(event.data.object, {
-      metadata: { type: "grant", userId: "clsncr3xx000010bbf8i4m4lo" },
-      transfer_group: "LJMQ2Qo7Au2wbdbbSs5cl",
-    });
-
     if (event.type === "payment_intent.succeeded") {
       const { metadata, transfer_group } = event.data.object;
 
+      if (metadata.type === TransferType.round) {
+        await db.round.update({
+          where: { id: transfer_group! },
+          data: {
+            fundedAmount: { increment: event.data.object.amount },
+          },
+        });
+      }
       if (metadata.type === TransferType.grant) {
         if (!transfer_group) {
           throw new Error("Transfer group not found in payment");
         }
-
-        const contributions = await db.contribution.findMany({
-          where: { transferGroup: transfer_group },
-          include: { grant: true },
-        });
-
-        console.log(contributions);
-
-        /*
-        The payment is not available in the balance right away.
-        Will the platform hold the payments until available?
-        There's a balance updated webhook. 
-
-        Another option is to top up the balance to cover until tx is approved.
-
-        */
-        // await Promise.all(
-        //   contributions.map((contribution) =>
-        //     stripe.transfers.create({
-        //       amount: contribution.amount * 100,
-        //       currency: "usd",
-        //       destination: contribution.grant.stripeAccount!,
-        //       transfer_group,
-        //     }),
-        //   ),
-        // );
-
-        // console.log(`Transfer successful to ${contributions.length} grants!`);
-        // await db.contribution.updateMany({
-        //   where: { transferGroup: transfer_group },
-        //   data: { status: "success" },
-        // });
       }
-
-      // if (event.type === "checkout.session.completed") {
-      //   const { metadata } = event.data.object;
-      //   console.log({ metadata });
-      // }
     }
     return NextResponse.json({ result: event, ok: true });
   } catch (error) {
