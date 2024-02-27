803bec89035b3199207ed821b196f8e906a47ab0
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-02-27 12:40:12 +0100

    Add funded amount to round card

--- src/app/rounds/_components/discover-rounds.tsx
+++ src/app/rounds/_components/discover-rounds.tsx
@@ -8,6 +8,7 @@ import { useDebouncedFilter } from "~/hooks/useFilter";
 import { distributionTypeLabels } from "~/server/api/routers/round/round.schemas";
 import { api } from "~/trpc/react";
 import { endsIn, formatDate } from "~/utils/date";
+import { formatMoney } from "~/utils/formatMoney";
 
 export function DiscoverRounds({}) {
   const { sortBy, order, search } = useDebouncedFilter();
@@ -33,13 +34,16 @@ function RoundCard({
   distributionType,
   startsAt,
   endsAt,
+  fundedAmount,
+  currency,
 }: Round) {
   return (
     <Link href={`/rounds/${id}`} className="rounded-xl border">
-      <div className="relative aspect-square ">
+      <div className="relative aspect-video ">
         <Badge className="absolute right-2 top-2 z-10">
           Ends in {endsIn(endsAt)}
         </Badge>
+
         <Image
           alt={name}
           src={image}
@@ -51,8 +55,14 @@ function RoundCard({
           }}
         />
       </div>
-      <div className="px-2 py-3">
-        <h3 className="mb-2 truncate text-lg font-semibold">{name}</h3>
+      <div className="space-y-2 px-2 py-3">
+        <h3 className="truncate text-lg font-semibold">{name}</h3>
+        <div className="flex items-center gap-2">
+          <div className="text-xl font-semibold">
+            {formatMoney(fundedAmount, currency)}
+          </div>
+          <div>funded</div>
+        </div>
         <div className="flex flex-wrap gap-1">
           <Badge>
             <FunctionSquare className="size-4" />
